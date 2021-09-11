754314106a9d160dcc15c80fe90eb0835ed9fdbd
LunarMoon72 <88513736+LunarMoon72@users.noreply.github.com>
2021-09-11 10:04:57 -0400

    that should do it i hope

--- src/LunarMoon72/Plugin5/Main.php
+++ src/LunarMoon72/Plugin5/Main.php
@@ -30,14 +30,14 @@ class Main extends PluginBase {
     }
 
     public function mainui($player) {
-        $form = $this->getServer()->getPluginManager()->getPlugin("FormAPI")->createSimpleForm(function (Player $player, int $data = null){
-            if($data === null) {
+        $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+        $form = $api->createSimpleForm(function (Player $player, int $data = null){
+            if($data === null){
                 return true;
             }
-
         });
         $form->setTitle("Info");
-        $form-setContent("Welcome " . $player->getName() . ". Do /is create to make an island. Do /isui to manage your island in UI! Do /bs to open the bookshop and do /craft to open the craft menu");
+        $form->setContent("Welcome " . $player->getName() . ". Do /is create to make an island. Do /isui to manage your island in UI! Do /bs to open the bookshop and do /craft to open the craft menu");
         $form->addButton("Back");
         $form->sendToPlayer($player);
         return $form;

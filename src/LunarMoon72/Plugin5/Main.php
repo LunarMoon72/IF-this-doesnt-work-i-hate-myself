4722db2a998c1ca2579eaf8efebde694fd0934fa
LunarMoon72 <88513736+LunarMoon72@users.noreply.github.com>
2021-09-11 10:00:05 -0400

    Add files via upload

---
+++ src/LunarMoon72/Plugin5/Main.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace LunarMoon72\Plugin5;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\Player;
+use pocketmine\Server;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class Main extends PluginBase {
+    
+    public function onEnabled() : void {
+        $this->getLogger()->info("§bPlugin Enabled");
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args) : bool {
+        switch($cmd->getName()) {
+            if($sender instanceof Player) {
+                $this->mainui($sender);
+            } else {
+                $this->getLogger()->info("Use this In game");
+            }
+
+        }
+        return true;
+    }
+
+    public function mainui($player) {
+        $form = $this->getServer()->getPluginManager()->getPlugin("FormAPI")->createSimpleForm(function (Player $player, int $data = null){
+            if($data === null) {
+                return true;
+            }
+
+        });
+        $form->setTitle("Info");
+        $form-setContent("Welcome " . $player->getName() . ". Do /is create to make an island. Do /isui to manage your island in UI! Do /bs to open the bookshop and do /craft to open the craft menu");
+        $form->addButton("Back");
+        $form->sendToPlayer($player);
+        return $form;
+    }
+}
